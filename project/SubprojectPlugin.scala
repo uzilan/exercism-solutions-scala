47b50c72330f6d3f3469d36d67d0d8edb9ff7368
Uzi Landsmann <ulandsmann@invidi.se>
2019-02-25 21:00:18 +0100

    Ignoring .git when generating subprojects

--- project/SubprojectPlugin.scala
+++ project/SubprojectPlugin.scala
@@ -4,7 +4,7 @@ object SubprojectPlugin extends AutoPlugin {
    override val requires = sbt.plugins.JvmPlugin
    override val trigger = allRequirements
    override lazy val extraProjects: Seq[Project] = {
-      val dirs = (file(".") * ("*" -- "project" -- "target" -- ".idea")) filter { _.isDirectory }
+      val dirs = (file(".") * ("*" -- "project" -- "target" -- ".idea" -- ".git")) filter { _.isDirectory }
       dirs.get.toList map { dir =>
          Project(dir.getName.replaceAll("""\W""", "_"), dir)
       }

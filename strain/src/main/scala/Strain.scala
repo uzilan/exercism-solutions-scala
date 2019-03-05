c957200d8be97203f90ae8e34594305f41e23bf5
Uzi Landsmann <uzi.landsmann@gmail.com>
2019-03-05 22:34:57 +0100

    Refactored scala/strain

--- strain/src/main/scala/Strain.scala
+++ strain/src/main/scala/Strain.scala
@@ -1,11 +1,9 @@
 object Strain {
    def keep[T](seq: Seq[T], predicate: T => Boolean): Seq[T] =
       seq.foldLeft(Seq[T]()) {
-         (acc, item) => if (predicate(item)) acc :+ item else acc
-      }
+         (acc, item) => if (predicate(item)) item +: acc else acc
+      }.reverse
 
    def discard[T](seq: Seq[T], predicate: T => Boolean): Seq[T] =
-      seq.foldLeft(Seq[T]()) {
-         (acc, item) => if (predicate(item)) acc else acc :+ item
-      }
+      keep[T](seq, !predicate(_))
 }

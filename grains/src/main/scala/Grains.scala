fb6b138988decd3027ec0a90389b55bd8c39c82b
Uzi Landsmann <uzi.landsmann@gmail.com>
2019-03-06 22:27:01 +0100

    Updated scala/grains

--- grains/src/main/scala/Grains.scala
+++ grains/src/main/scala/Grains.scala
@@ -5,5 +5,5 @@ object Grains {
       if (sq <= 0 || sq > 64) None
       else Some(BigInt(2).pow(sq - 1))
 
-   def total: BigInt = (0 until 64).foldLeft(BigInt(1)) { (acc, _) => acc * 2 } - 1
+   def total: BigInt = BigInt(2).pow(64) - 1
 }
